eb225229f33e3ae81f1e318eb213c329c48bcf2f
woshixiaotugou <2117425141@qq.com>
2021-05-10 11:38:44 +0800

    小测试

--- hzpt-jurisdiction/src/main/java/com/inet/App.java
+++ hzpt-jurisdiction/src/main/java/com/inet/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!我是傻狗阿三大苏打" );
+        System.out.println( "Hello World!我是傻狗阿三大苏打艾昂" );
     }
 }
