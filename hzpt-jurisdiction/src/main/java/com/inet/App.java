b743114b34adcfa80429adf27c4cd1fb638b00ff
woshixiaotugou <2117425141@qq.com>
2021-05-10 11:33:06 +0800

    小测试

--- hzpt-jurisdiction/src/main/java/com/inet/App.java
+++ hzpt-jurisdiction/src/main/java/com/inet/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!hhasdas" );
+        System.out.println( "Hello World!我是傻狗" );
     }
 }
