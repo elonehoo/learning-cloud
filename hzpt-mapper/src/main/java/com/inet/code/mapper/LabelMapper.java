f27e65b406d3ab60dcdc1dabf3fd04457967d36e
FanHoozy <fyhz0723@163.com>
2021-05-17 22:13:07 +0800

    fyhz-update-update LabelMapper

--- hzpt-mapper/src/main/java/com/inet/code/mapper/LabelMapper.java
+++ hzpt-mapper/src/main/java/com/inet/code/mapper/LabelMapper.java
@@ -15,5 +15,11 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface LabelMapper extends BaseMapper<Label> {
-
+    /**
+     * 根据题目id修改标签表的网站名name
+     * @param id 需要修改的题目id
+     * @param name 需要修改的网站名
+     * @return 是否修改成功
+     */
+    boolean updateLabelNameByTopicId(String id, String name);
 }
